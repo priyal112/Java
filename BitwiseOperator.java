e7077300f2dbeee63f979484cd72e49483fc2035
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-02-17 19:33:04 +0530

    operators

---
+++ BitwiseOperator.java
@@ -0,0 +1,32 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        // AND OPERATOR
+        int a = 5 & 4;  
+        System.out.println(Integer.toBinaryString(5));
+        System.out.println(Integer.toBinaryString(4));
+        System.out.println(a);
+        System.out.println(Integer.toBinaryString(a));
+        
+        // OR OPERATOR
+        int b = 3 | 5;
+        System.out.println(b);
+
+        // XOR OPERATOR
+        int c = 8 ^ 3;
+        System.out.println(c);
+
+        // NOT OPERATOR
+        int y = 5;
+        int x = ~y;
+        System.out.println(x);
+
+        // LEFT SHIFT
+        int z = y << 1;
+        System.out.println(z);
+
+        // RIGHT SHIFT
+        int ab = y >> 2;
+        System.out.println(ab);
+
+    }
+}
--- Increment_Decrement_Operator.java
+++ Increment_Decrement_Operator.java
@@ -1,6 +1,6 @@
 public class Increment_Decrement_Operator {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 1;
 
         System.out.println(a + a++);
         System.out.println(a + ++a);
