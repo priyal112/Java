fe85caf6a1c2cb4db2422f91f20da2adeb06bcf2
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-02-27 16:15:19 +0530

    sk

--- Static_Keyword/Test.java
+++ Static_Keyword/Test.java
@@ -3,10 +3,10 @@ package Static_Keyword;
 public class Test {
     public static void main(String[] args) {
         
-        Student student1 = new Student();
+        /* Student student1 = new Student();
         Student student2 = new Student();
         Student student3 = new Student();
-        Student student4 = new Student();
+        Student student4 = new Student(); */
 
         System.out.println(Student.count);
         Student.getCount();
