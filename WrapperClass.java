f23b7eaa0a556fee3d1efa0affdff6aabfd7c8aa
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-02-28 19:29:18 +0530

    wrapper class

---
+++ WrapperClass.java
@@ -0,0 +1,15 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        /* int a = 1;
+        Integer b = 1;   // class
+
+        boolean isAdult = true;
+        Boolean hasCard = false;  */
+        
+        // METHODS
+        System.out.println(Integer.max(1, 3));
+        System.out.println(Integer.toBinaryString(10));
+
+
+    }
+}
