4dcd587fce2d7c2dffdeb7d5abc7ec0b193c1b7d
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-03-29 21:19:21 +0530

    vector

---
+++ Vector/vector_demo.java
@@ -0,0 +1,10 @@
+package Vector;
+
+import java.util.Vector;
+
+public class vector_demo {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        System.out.println(vector.capacity());
+    }
+}
