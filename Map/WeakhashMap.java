f4e1ec705678037fcbef31a60591cf74b2798ea2
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-04-12 19:44:18 +0530

    weak_hashmap

---
+++ Map/WeakhashMap.java
@@ -0,0 +1,40 @@
+package Map;
+
+import java.util.WeakHashMap;
+
+public class WeakhashMap {
+    public static void main(String[] args) {
+        WeakHashMap<String, Image> imageCache = new WeakHashMap<>();
+        imageCache.put("img1", new Image("Image 1"));
+        imageCache.put("img2", new Image("Image 2"));
+        System.out.println(imageCache);
+        System.gc();
+        simulateApplicationRunning();
+        System.out.println("Cache after running (some entries my be cleared): " + imageCache);
+    }
+
+    private static void simulateApplicationRunning() {
+        try {
+            System.out.println("Simulating application running");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+             e.printStackTrace();
+        }
+    }
+
+}
+
+class Image {
+    private String name;
+
+    public Image(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "[name=" + name + "]";
+    }
+
+    
+}
