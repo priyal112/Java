80859fd9d7b0fb7f82333d67c119a79c621da7dc
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-04-13 19:46:50 +0530

    identityHashmap

---
+++ Map/IdentityHashMap.java
@@ -0,0 +1,17 @@
+package Map;
+
+//import java.util.IdentityHashMap;
+import java.util.Map;
+
+public class IdentityHashMap {
+    public static void main(String[] args) {
+        String key1 = new String("Key");
+        String key2 = new String("Key");
+
+        Map<String, Integer> map = new java.util.IdentityHashMap<>();
+        map.put(key1, 1);
+        map.put(key2, 2);
+        System.out.println(key1.equals(key2));
+        System.out.println(map);
+    }
+}
