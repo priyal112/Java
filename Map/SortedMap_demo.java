ad21f592c6a1cd073b9bda610d429b893c2d4c50
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2026-04-22 21:39:27 +0530

    map

---
+++ Map/SortedMap_demo.java
@@ -0,0 +1,12 @@
+package Map;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMap_demo {
+  SortedMap<String, Integer> map = TreeMap<>();
+  map.put("Alex", 98);
+  map.put("Shubham", 80);
+  map.put("Raj", 78);
+  System.out.println(map);
+}
